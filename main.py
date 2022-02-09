2fa6aba874d1d2dce1fbd89f4de873f5ac95ee80
Pavel Shilov <pavel.s.shilov@gmail.com>
2022-02-09 15:02:56 +0300

    update searching format for different package

--- main.py
+++ main.py
@@ -1,19 +1,52 @@
 import requests
 import json
-#import pandas as pd
+from collections import ChainMap
 
 
 first_method = 'sisyphus'
 request_url_sisyphus = 'https://rdb.altlinux.org/api/export/branch_binary_packages/' + first_method
-response = requests.get(request_url_sisyphus)
-sisyphus_text = response.text
-sisyphus_dict=json.loads(sisyphus_text)
-#print(sisyphus_dict['packages'])
+response_sisyphus = requests.get(request_url_sisyphus).text
+sisyphus_dict = json.loads(response_sisyphus)
+sisyphus_dictionary = sisyphus_dict['packages']
 
 second_method = 'p10'
 request_url_p10 = 'https://rdb.altlinux.org/api/export/branch_binary_packages/' + second_method
-response = requests.get(request_url_p10)
-p10_text = response.text
-p10_dict = json.loads(p10_text)
-print(p10_dict['packages'])
+response_p10 = requests.get(request_url_p10).text
+p10_dict = json.loads(response_p10)
+p10_dictionary = p10_dict['packages']
 
+p10_diff = []
+sisyphus_diff = []
+
+for obj in sisyphus_dictionary:
+    for data in p10_dictionary:
+        if obj['name'] != data['name']:
+            p10_diff.append(obj)
+            break
+        else:
+            break
+
+for obj in p10_dictionary:
+    for data in sisyphus_dictionary:
+        if obj['name'] != data['name']:
+            sisyphus_diff.append(obj)
+            break
+        else:
+            break
+
+
+
+print(p10_diff)
+print(sisyphus_diff)
+'''
+for name in data_collection():
+    p10_diff = []
+    sisyphus_diff = []
+    if name.p10_dictionary != name.sisyphus_dictionary:
+        p10_diff.append(name.p10_dictionary)
+        print(p10_diff)
+    elif sisyphus_dictionary['name'] != p10_dictionary['name']:
+        sisyphus_diff.append(sisyphus_dictionary['name'])
+        print(sisyphus_diff)
+
+'''
